d599b0473deb6583ea13ea41b2186aed89112482
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-24 22:12:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import pandas as pd  # Import the pandas library for data manipulation
 
 # Ask the user to input a word and convert it to uppercase
-name = input("Please enter a word: ").upper()
+fix= True
+while fix:
+    name = input("Please enter a word: ").upper()
 
-# Create a list comprehension to store each letter of the input word
-name_list = [letter for letter in name]
+    # Create a list comprehension to store each letter of the input word
+    name_list = [letter for letter in name]
 
-# Read the NATO phonetic alphabet data from the CSV file into a DataFrame
-data = pd.read_csv("nato_phonetic_alphabet.csv")
+    # Read the NATO phonetic alphabet data from the CSV file into a DataFrame
+    data = pd.read_csv("nato_phonetic_alphabet.csv")
 
-# Create a dictionary comprehension to map each letter to its corresponding phonetic code
-data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+    # Create a dictionary comprehension to map each letter to its corresponding phonetic code
+    data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-# Use list comprehension to generate a list of phonetic codes for each letter in the input word
-phonetic_code = [data_dict[key] for key in name_list]
+    try:
 
-# Print the list of phonetic codes
-print(phonetic_code)
+        # Use list comprehension to generate a list of phonetic codes for each letter in the input word
+        phonetic_code = [data_dict[key] for key in name_list]
+
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+
+    else:
+        # Print the list of phonetic codes
+        print(phonetic_code)
+
+
